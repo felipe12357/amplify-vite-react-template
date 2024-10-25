b754d23b3ad1649f560c463694ea61fc7f4e6113
Andres <andrewt12357@hotmail.com>
2024-10-25 07:37:05 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import { Authenticator } from '@aws-amplify/ui-react';
+// import { Authenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(outputs);
 
